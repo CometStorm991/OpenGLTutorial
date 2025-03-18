0f3f112989fb3542d9dcf4be9c8196fefbcf1476
CometStorm991 <cometstorm991@gmail.com>
2025-03-18 17:24:00 -0400

    Added OpenGL debugging and multiple textures

--- OpenGLTutorial/Shaders/FragmentShader.glsl
+++ OpenGLTutorial/Shaders/FragmentShader.glsl
@@ -5,9 +5,13 @@ in vec2 textureCoordinates;
 layout(location = 0) out vec4 color;
 
 uniform float redColor;
-uniform sampler2D inputTexture;
+
+uniform sampler2D inputTexture0;
+uniform sampler2D inputTexture1;
 
 void main() {
-	// color = vec4(textureCoordinates.x, textureCoordinates.y, 1.0f, 1.0f);
-	color = texture(inputTexture, textureCoordinates);
+	vec4 gradientColor = vec4(textureCoordinates.x, textureCoordinates.y, 1.0f, 1.0f);
+	vec4 texture0Color = texture(inputTexture0, textureCoordinates);
+	vec4 texture1Color = texture(inputTexture1, textureCoordinates);
+	color = mix(texture0Color * gradientColor, texture1Color, 0.5);
 }
--- OpenGLTutorial/Source/Application.cpp
+++ OpenGLTutorial/Source/Application.cpp
@@ -20,6 +20,83 @@ uint64_t getMillisecondsSinceTimePoint(std::chrono::time_point<ClockType> start)
     return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
 }
 
+uint32_t loadTexture(const std::string& imagePath, GLenum textureUnit)
+{
+    int width, height, channelCount;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(imagePath.c_str(), &width, &height, &channelCount, 0);
+    if (!data)
+    {
+        std::cout << "[Error] Failed to load texture" << std::endl;
+    }
+
+    uint32_t textureId;
+    glGenTextures(1, &textureId);
+    glActiveTexture(textureUnit);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return textureId;
+}
+
+// This code is from learnopengl.com
+void APIENTRY glDebugOutput(GLenum source,
+    GLenum type,
+    unsigned int id,
+    GLenum severity,
+    GLsizei length,
+    const char* message,
+    const void* userParam)
+{
+    // ignore non-significant error/warning codes
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -28,8 +105,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 360, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -43,7 +122,15 @@ int main(void)
         std::cout << "Glew failed." << std::endl;
     }
 
-    //std::cout << glGetString(GL_VERSION) << std::endl;
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    }
 
     float positions[16] = {
         -0.5f, -0.5f, 0.0f, 0.0f,
@@ -57,13 +144,6 @@ int main(void)
         2, 3, 1
     };
 
-    int width, height, channelCount;
-    unsigned char* data = stbi_load("Resources/NeutronStar.jpg", &width, &height, &channelCount, 0);
-    if (!data)
-    {
-        std::cout << "[Error] Failed to load texture" << std::endl;
-    }
-
     uint32_t vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
@@ -74,20 +154,8 @@ int main(void)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), elements, GL_STATIC_DRAW);
 
-    uint32_t texture;
-    glGenTextures(1, &texture);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    stbi_image_free(data);
+    uint32_t texture0 = loadTexture("Resources/NeutronStar.jpg", GL_TEXTURE0);
+    uint32_t texture1 = loadTexture("Resources/ArchLinux.jpeg", GL_TEXTURE1);
 
     uint32_t vao;
     glGenVertexArrays(1, &vao);
@@ -104,7 +172,7 @@ int main(void)
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    
     
     std::shared_ptr<Shader> vertexShader = std::make_shared<Shader>(GL_VERTEX_SHADER, "Shaders/VertexShader.glsl");
     std::shared_ptr<Shader> fragmentShader = std::make_shared<Shader>(GL_FRAGMENT_SHADER, "Shaders/FragmentShader.glsl");
@@ -114,28 +182,41 @@ int main(void)
     program.load();
     program.unuse();
 
-    
-    // Have to keep element array buffer bound for drawing
+    program.use();
+    glUniform1i(glGetUniformLocation(program.getId(), "inputTexture0"), 0);
+    glUniform1i(glGetUniformLocation(program.getId(), "inputTexture1"), 1);
+    program.unuse();
+
+    std::cout << glGetError() << std::endl;
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.use();
+
         uint32_t milliseconds = getMillisecondsSinceTimePoint(start);
         float redColor = std::sin(milliseconds / 1000.0f * 2.0f * std::numbers::pi_v<float>) / 2.0f + 0.5f;
 
         int redColorLocation = glGetUniformLocation(program.getId(), "redColor");
         glUniform1f(redColorLocation, redColor);
 
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        program.use();
-        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
